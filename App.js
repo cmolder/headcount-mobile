96a1538de3d098a1c5c48cdefd2c507f885235ee
City303 <City303@users.noreply.github.com>
2019-10-31 14:03:15 -0500

    Fixed Header and added SafeAreaViews to avoid notch blocking the content

--- App.js
+++ App.js
@@ -1,20 +1,33 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Fragment } from 'react';
+import { StyleSheet, Platform, StatusBar, SafeAreaView, View } from 'react-native';
 import Header from './components/Header';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Header text="Headcount" />
-    </View>
+    <Fragment>
+
+      {/* Notch/top region for iOS, otherwise size 0 */}
+      <SafeAreaView style={styles.notch}>
+        <StatusBar barStyle="light-content"/>
+      </SafeAreaView>
+
+       {/* Remainder of app view, including home bar for iOS */}
+      <SafeAreaView style={styles.bottom}>
+        <Header text="Headcount" />
+      </SafeAreaView>
+    
+    </Fragment>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  notch: {
+    flex: 0,
+    backgroundColor: '#171717',
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
   },
+  bottom: {
+    flex: 1,
+    backgroundColor: '#bbb'
+  }
 });
